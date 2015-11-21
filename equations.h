5be7a44d0a6ea731469c5af71624cf03762ba6ab
h3nnn4n <uber.renan@gmail.com>
2015-11-20 23:32:50 -0200

    Clifford attractor is now the default

--- equations.h
+++ equations.h
@@ -1,9 +1,9 @@
 #ifndef X_EQUATION
-#define X_EQUATION xn = (a * cos((j * M_PI/180.0) + c)) + b * sinh(y * d) / cosh(x * y * a) + a * cos( x * c - d) + b;
+#define X_EQUATION xn = sin(a * y) + c * cos(a * x);  // xn = (a * cos((j * M_PI/180.0) + c)) + b * sinh(y * d) / cosh(x * y * a) + a * cos( x * c - d) + b;
 #endif
 
 #ifndef Y_EQUATION
-#define Y_EQUATION yn = (b * sin((j * M_PI/180.0) + d)) + c * sinh(x * a) / cosh(x * y * b) + b * sin( y * d - a) + c;
+#define Y_EQUATION yn = sin(b * x) + d * cos(b * y);  // yn = (b * sin((j * M_PI/180.0) + d)) + c * sinh(x * a) / cosh(x * y * b) + b * sin( y * d - a) + c;
 #endif
 
 #ifndef W_EQUATION
@@ -13,4 +13,3 @@
 #ifndef Z_EQUATION
 #define Z_EQUATION
 #endif
-
