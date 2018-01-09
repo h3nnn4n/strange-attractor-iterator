184b53b2ad0f8c320101ad163b02957c02a18ce0
h3nnn4n <uber.renan@gmail.com>
2018-01-09 18:20:48 -0200

    wip on interpreter

---
+++ interpreter.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    while True:
+        line = input()
+        print(line)
--- strange.py
+++ strange.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3.5
-# -*- coding: utf-8 -*-
-
 import ctypes
 import math
 import color
